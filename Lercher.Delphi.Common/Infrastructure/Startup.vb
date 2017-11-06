2f6b0866e297b5e891a8caed62acbd55b2fe49b8
Martin Lercher <lercher@singhammer.de>
2017-11-06 17:34:47 +0100

    Caching now based on Etags for Chrome (Cache-Control: max-age=0 header)

--- Lercher.Delphi.Common/Infrastructure/Startup.vb
+++ Lercher.Delphi.Common/Infrastructure/Startup.vb
@@ -1,5 +1,6 @@
 ﻿Imports Owin
 Imports System.Web.Http
+Imports Microsoft.Owin
 Imports Microsoft.Owin.Cors
 Imports Microsoft.Owin.StaticFiles
 Imports Microsoft.Owin.FileSystems
@@ -13,11 +14,18 @@ Public Class Startup
 
         ' http://odetocode.com/blogs/scott/archive/2014/02/10/building-a-simple-file-server-with-owin-and-katana.aspx
         ' http://docs.asp.net/en/latest/fundamentals/static-files.html
-        Dim webroot = IO.Path.Combine(IO.Path.GetDirectoryName( Reflection.Assembly.GetExecutingAssembly.Location),  "wwwroot")
+        Dim webroot = IO.Path.Combine(IO.Path.GetDirectoryName(Reflection.Assembly.GetExecutingAssembly.Location), "wwwroot")
 #If DEBUG Then
         webroot = "\daten\Lercher.Delphi\Lercher.Delphi.Common\wwwroot"
 #End If
+
         Console.WriteLine("Serving static files from: {0}", webroot)
+        AppBuilderUseExtensions.Use(app,
+            Async Function(context As IOwinContext, nxt As Func(Of Task)) As Task
+                Await nxt()
+                context.Response.Headers.Add("Cache-Control", {"max-age=0"}) ' For Chrome to honor ETags
+            End Function
+        )
         app.UseFileServer(New FileServerOptions() With {.FileSystem = New PhysicalFileSystem(webroot)})
     End Sub
 
